edae1ce0f6a53238c263ee382d26773900f96664
Vadigi789 <vadigivenkatesh789@gmail.com>
2026-05-18 19:21:41 +0530

    second-commit

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "devops/terraform.tfstate"
-    region         = "ap-south-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
